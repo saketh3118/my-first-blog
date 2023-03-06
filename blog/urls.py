117f11986d7e8241f14bf569b7071eea4584054b
saketh <lokasakethreddy789@gmail.com>
2023-03-06 21:52:08 +0530

    adding css and extending templates

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from blog import views
 from django.urls import path,include
 
 urlpatterns = [
+     path('post/<str:pk>/', views.post_detail, name='post_detail'),
     path('', views.post_list, name='post_list')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from blog.models import Blog 
 from django.utils import timezone
 # Create your views here.
 def post_list(request):
     post=Blog.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request,'post_list.html',{'posts':post})
+def post_detail(request,pk):
+    post = get_object_or_404(Blog,pk=pk)
+    return render(request,'post_detail.html', {'post': post})
